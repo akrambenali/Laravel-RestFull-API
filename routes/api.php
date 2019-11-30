c0d0a2d64586bddc44edb76096c8436f3821ad3e
Akram Benali <akram1304@gmail.com>
2019-11-30 22:52:47 +0100

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', 'API\RegisterController@register');
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('products', 'API\ProductController');
+});
